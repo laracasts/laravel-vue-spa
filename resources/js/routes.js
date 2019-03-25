519996f02cad3329f6be2f8a3be892301e394e76
Jeffrey Way <jeffrey@jeffrey-way.com>
2019-03-25 14:16:42 -0400

    Episode 6

--- resources/js/routes.js
+++ resources/js/routes.js
@@ -4,9 +4,11 @@ import Colors from './components/Colors';
 import Typography from './components/Typography';
 import Mascot from './components/Mascot';
 import Illustrations from './components/Illustrations';
-import LoadersAndAnimations from './components/LoadersAndAnimations';
 import Wallpapers from './components/Wallpapers';
 import NotFound from './components/NotFound';
+
+let LoadersAndAnimations = () =>
+    import(/* webpackChunkName: "loaders-and-animations.bundle" */ './components/LoadersAndAnimations');
 
 export default {
     mode: 'history',
